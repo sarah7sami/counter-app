a6e2503a9eca148238cd287bed7a83d010c05990
Sarah Sami <sarah7sami@gmail.com>
2023-01-29 13:01:29 -0500

    Initial Commit

---
+++ counter-app/counter_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  counter_appApp.swift
+//  counter-app
+//
+//  Created by Sarah Sami on 2023-01-29.
+//
+
+import SwiftUI
+
+@main
+struct counter_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
